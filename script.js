a491bd46ed534c1de33a5f732c86b6f7336a966c
abcjjj <johnaotao@hotmail.com>
2013-10-30 12:35:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
- 
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+function Menu() {
+    var startButton;
+    var quitButton;
+    var title;
+    
+    function setStartButton() {
+        startButton = document.getElementById("start");
+    }// end mutator
+    
+    function getStartButton() {
+        return startButton;
+    }// end accessor
+    
+    function setQuitButton() {
+        quitButton = document.getElementById("quit");
+    }// end mutator
+    
+    function getQuitButton() {
+        return quitButton;
+    }// end acessor
+    
+    function setTitle() {
+        title = document.getElementById("title");
+    }// end mutator
+    
+    function getTitle() {
+        return title;
+    }// end accessor
+    
+    function quitGame() {
+    // needs a BBUI function
+    }// end quitGame
+    
+    function showMenu() {
+        this.getStartButton().style.visibility = "visible";
+        this.getQuitButton().style.visibility = "visible";
+        this.getTitle().style.visibility = "visible";
+    }// end showMenu
+    
+    function hideMenu() {
+        this.getStartButton().style.visibility = "hidden";
+        this.getQuitButton().style.visibility = "hidden";
+        this.getTitle().style.visibility = "hidden";
+    }// end hideMenu
+    
+    function startGame() {
+        this.hideMenu();
+        
+    }// end startGame
+    
+}// end Constructor
