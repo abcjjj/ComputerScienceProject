9ae38545d8af6771676c6d6df66aab8399066e56
abcjjj <johnaotao@hotmail.com>
2013-10-31 12:34:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 
 /**
  * the Menu Screen
+ * all elements are visible initially
  */
 function Menu() {
     this.startButton;
@@ -65,22 +66,85 @@ menu.getQuitButton().onclick = menu.quitGame();
  * the character that the user controls
  */
 function Character() {
-
+    this.money = 0;
+    
+    function getMoney() {
+        return this.money;
+    }// end accessor
+    
+    function setMoney(money) {
+        this.money = money;
+    }// end mutator
     
 }// end Constructor
 
-//initializes character (should be at another place?)
+//initializes character 
+    //(should be at another place?)
 
 // action events
-
-
+    // none
 
 
 /**
  * the Transition Screen
+ * elements become visible accordingly
  */
 function Transition() {
-
+    this.instructions;
+    this.chooseToSkip;
+    this.win;
+    this.toNextSreen;
+    
+    function getInstructions() {
+        return this.instructions;
+    }// end accessor
+    
+    function setInstructions() {
+        this.instructions = document.getElementById("instructions");
+    }// end mutator
+    
+    function getChooseToSkip() {
+        return this.chooseToSkip;
+    }// end accessor
+    
+    function setChooseToSkip() {
+        this.chooseToSkip = document.getElementById("chooseToSkip");
+    }// end mutator
+    
+    function getWin() {
+        return this.win;
+    }// end accessor
+    
+    function setWin() {
+        this.win = document.getElementById("win");
+    }// end mutator
+    
+    function getToNextScreen() {
+        return this.toNextScreen
+    }// end accessor
+    
+    function setToNextScreen() {
+        this.toNextScreen = document.getElementById("toNextSccreen");
+    }// end mutator
+    
+    function typeDisplayed(type) {
+        if(type==="instructions")
+            this.getInstructions().style.visibility = "visible";
+        if(type==="before instructions")
+            this.getChooseToSkip().style.visibility = "visible";
+        if(type==="win")
+            this.getWin().style.visibility = "visible";
+        this.getToNextScreen().style.visibility = "visible";
+    }// end typeDisplayed
+    
+    function proceed(type) {
+        if(type==="instructions")
+            document.getElementById("battle").style.visibility = "visible";
+        if(type==="before instructions")
+            document.getElementById("battle").style.visibility = "visible";
+        if(type==="win")
+            document.getElementById("menu").style.visibility = "visible";
+    }// end proceed
     
 }// end Constructor
 
