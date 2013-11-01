8d3aa894c2663fb1c4ed1a6c7644de17f5b2d097
abcjjj <johnaotao@hotmail.com>
2013-11-01 12:31:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,33 +9,9 @@
  * all elements are visible initially
  */
 function Menu() {
-    this.startButton = -1;
-    this.quitButton= -1;
-    this.title= -1;
-    
-    function setStartButton() {
-        this.startButton = document.getElementById("start");
-    }// end mutator
-    
-    function getStartButton() {
-        return this.startButton;
-    }// end accessor
-    
-    function setQuitButton() {
-        this.quitButton = document.getElementById("quit");
-    }// end mutator
-    
-    function getQuitButton() {
-        return this.quitButton;
-    }// end acessor
-    
-    function setTitle() {
-        this.title = document.getElementById("title");
-    }// end mutator
-    
-    function getTitle() {
-        return this.title;
-    }// end accessor
+    this.startButton = document.getElementById("start");
+    this.quitButton= document.getElementById("quit");
+    this.title= document.getElementById("title");
     
     function quitGame() {
     // needs a BBUI function
@@ -44,7 +20,6 @@ function Menu() {
     function startGame() {
         document.getElementbyId("menu").style.visibility = "hidden";
         document.getElementbyId("transition").style.visibility = "visible";
-        // make new character (?)
         var character = new Character();
     }// end startGame
     
@@ -53,9 +28,6 @@ function Menu() {
 //Menu
 //initializes menu
 var menu = new Menu();
-menu.setTitle();
-menu.setQuitButton();
-menu.setStartButton();
 
 // action events
 menu.getStartButton().onclick = menu.startGame();
@@ -68,18 +40,14 @@ menu.getQuitButton().onclick = menu.quitGame();
 function Character() {
     this.money = 0;
     
-    function getMoney() {
-        return this.money;
-    }// end accessor
-    
     function setMoney(money) {
         this.money = money;
-    }// end mutator
+    }// end mutatorbe
     
 }// end Constructor
 
 //initializes character 
-    //(should be at another place?)
+    //(should go to another place?)
 
 // action events
     // none (so far)
@@ -90,46 +58,14 @@ function Character() {
  * elements become visible accordingly
  */
 function Transition() {
-    this.instructions = -1;
-    this.chooseToSkip = -1;
-    this.win = -1;
-    this.toNextSreen = -1;
-    
-    function getInstructions() {
-        return this.instructions;
-    }// end accessor
-    
-    function setInstructions() {
-        this.instructions = document.getElementById("instructions");
-    }// end mutator
-    
-    function getChooseToSkip() {
-        return this.chooseToSkip;
-    }// end accessor
-    
-    function setChooseToSkip() {
-        this.chooseToSkip = document.getElementById("chooseToSkip");
-    }// end mutator
-    
-    function getWin() {
-        return this.win;
-    }// end accessor
-    
-    function setWin() {
-        this.win = document.getElementById("win");
-    }// end mutator
-    
-    function getToNextScreen() {
-        return this.toNextScreen
-    }// end accessor
-    
-    function setToNextScreen() {
-        this.toNextScreen = document.getElementById("toNextSccreen");
-    }// end mutator
+    this.instructions = document.getElementById("instructions");
+    this.chooseToSkip = document.getElementById("chooseToSkip");
+    this.win = document.getElementById("win");
+    this.toNextSreen = this.toNextScreen = document.getElementById("toNextSccreen");
     
     function typeDisplayed(type) {
         if(type==="instructions")
-            this.getInstructions().style.visibility = "visible";
+            this.getanswerInstructions().style.visibility = "visible";
         if(type==="before instructions")
             this.getChooseToSkip().style.visibility = "visible";
         if(type==="win")
@@ -150,14 +86,11 @@ function Transition() {
 //Transition
 //initializes transition
 var transition = new Transition();
-transistion.setInstructions();
-transistion.setWin();
-transistion.setChooseToSkip();
-transistion.setToNextScreen();
 
 // action events
 transition.getChooseToSkip.onclick = transition.proceed("instructions");
 
+// @TO DO: figure out a way to let the screen proceed properly
 // if(transition.getInstructions().getAttribute("visibility")==="visible")
 //     transition.getToNextScreen.onclick = transition.proceed("instructions");
 
@@ -171,126 +104,24 @@ transition.getChooseToSkip.onclick = transition.proceed("instructions");
 function Battle() {
     this.answer = -1;
     this.isCharacterDead = false;
-    this.background = -1;
-    this.question = -1;
-    this.inputAnswer = -1;
-    this.monster = -1;
-    this.toShopButton = -1;
-    this.moneyValue = -1;
-    
-    function getAnswer() {
-        return this.answer;
-    }// end accessor
+    this.background = document.getElementById("backgroundCave");
+    this.question = document.getElementById("question");
+    this.inputAnswer = document.getElementById("inputAnswer");
+    this.monster = document.getElementById("monster");
+    this.toShopButton = document.getElementById("toShop");
+    this.moneyValue = document.getElementById("showMoney");
     
     function setAnswer(answer) {
         this.answer = answer;
     }// end mutator
     
-    function getIsCharacterKilled() {
-        return this.isCharacterDead;
-    }// end accessor
-    
     function setIsCharacterKilled(dead) {
         this.isCharacterDead = dead;
     }// end mutator
-    
-    function getBackground() {
-        return this.background;
-    }// end accessor
-    
-    function setBackground() {
-        this.background = document.getElementById("backgroundCave");
-    }// end mutator
-    
-    function getQuestion() {
-        return this.question;
-    }// end accessor
-    
-    function setQuestion() {
-        this.question = document.getElementById("question");
-    }// end mutator
-    
-    function getInputAnswer() {
-        return this.inputAnswer;
-    }// end accessor
-    
-    function setInputAnswer() {
-        this.inputAnswer = document.getElementById("inputAnswer");
-    }// end mutator
-    
-    function getMonster() {
-        return this.monster;
-    }// end accessor
-    
-    function setMonster() {
-        this.monster = document.getElementById("monster");
-    }// end mutator
-    
-    function getToShopButton() {
-        return this.toShopButton;
-    }// end accessor
-    
-    function setToShopButton() {
-        this.toShopButton = document.getElementById("toShop");
-    }// end mutator
-    
-    function getMoneyValue() {
-        return this.moneyValue;
-    }// end accessor
-    
-    function setMoneyValue() {
-        this.moneyValue = document.getElementById("showMoney");
-    }// end mutator
-    
+
     function createMonster(type) {
         // some code to change the monster...
     }// end createMonster
-    
-    function createQuestion(){
-        var questionString = "";
-        var terms = 2;
-        var numbers = new Array();
-        for(var i=0; i<terms; i++) {
-            numbers[i] = generateRandomNatrualNumber();
-            questionString += numbers[i];
-            if(i===0)
-                this.setAnswer(numbers[i]);
-            else {
-                if(questionString.charAt(questionString.length-2)==='+')
-                    this.setAnswer(this.getAnswer()+numbers[i]);
-                if(questionString.charAt(questionString.length-2)==='-')
-                    this.setAnswer(this.getAnswer()-numbers[i]);
-                if(questionString.charAt(questionString.length-2)==='*')
-                    this.setAnswer(this.getAnswer()*numbers[i]);
-                if(questionString.charAt(questionString.length-2)==='/')
-                    this.setAnswer(Math.round(this.getAnswer()/numbers[i]));// round to ones
-            }// end if-else
-            if(i!==terms-1)
-                questionString += this.generateRandomSign();
-        }
-        return questionString + " = ?";
-    }// end createQuestion
-    
-    function generateRandomSign() {
-        // random sign:
-        var randomSign = Math.floor(Math.random()*4);
-        if(randomSign == 0)
-            return " + ";
-        else if(randomSign == 1)
-            return " - ";
-        else if(randomSign == 2)
-            return " * ";
-        else //if(randomSign == 3)
-            return " / ";  
-    }// end generateRandomSign
-    
-    function generateRandomNatrualNumber() {
-        return "" + Math.round(Math.random()*100);
-    }// end generateRandomSign
-    
-    function isAnswerCorrect(input) {
-        return input===this.getAnswer();
-    }// end isAnswerCorrect
     
     function death() {
         // when something (?) is dead
@@ -301,58 +132,61 @@ function Battle() {
         document.getElementById("shop").style.visibility = "visible";
     }// end toShop
     
+    // make question
+    
 }// end Constructor
 
 //initializes battle screen 
-
+var battleScreen = new Battle();
 
 // action events
 
-// @TO DO: modify the above to the followings:
-    function generateRandomSign() {
-        // random sign:
-        var randomSign = Math.floor(Math.random()*4);
-        if(randomSign == 0)
-            return " + ";
-        else if(randomSign == 1)
-            return " - ";
-        else if(randomSign == 2)
-            return " * ";
-        else //if(randomSign == 3)
-            return " / ";  
-    }// end generateRandomSign
-    function generateRandomNatrualNumber() {
-        if(Math.floor(Math.random()*2)===1)
-            return "" + Math.round(Math.random()*100);// 2digits
-        else // 1digit
-            return "" + Math.round(Math.random()*10);
-    }// end generateRandomSign
+// @TO DO: add the followings to the above:
+function generateRandomSign() {
+    // random sign:
+    var randomSign = Math.floor(Math.random()*4);
+    if(randomSign == 0)
+        return " + ";
+    else if(randomSign == 1)
+        return " - ";
+    else if(randomSign == 2)
+        return " * ";
+    else //if(randomSign == 3)
+        return " / ";  
+}// end generateRandomSign
+
+function generateRandomNatrualNumber() {
+    if(Math.floor(Math.random()*2)===1)
+        return "" + Math.round(Math.random()*100);// 2digits
+    else // 1digit
+        return "" + Math.round(Math.random()*10);
+}// end generateRandomSign
     
-    var answer = 0;
-    function createQuestion(){
-        var questionString = "";
-        var terms = 2;
-        var numbers = new Array();
-        for(var i=0; i<terms; i++) {
-            numbers[i] = generateRandomNatrualNumber();
-            questionString += numbers[i];
-            if(i==0)
-               answer = numbers[i];
-            else {
-                if(questionString.charAt(questionString.length-numbers[i].length-2)==='+')
-                   answer = (parseInt(answer)+parseInt(numbers[i]));
-                if(questionString.charAt(questionString.length-numbers[i].length-2)==='-')
-                   answer = (parseInt(answer)-parseInt(numbers[i]));
-                if(questionString.charAt(questionString.length-numbers[i].length-2)==='*')
-                   answer = (parseInt(answer)*parseInt(numbers[i]));
-                if(questionString.charAt(questionString.length-numbers[i].length-2)==='/')// should round?
-                   answer = (parseInt(answer)/parseInt(numbers[i]));
-            }// end if-else
-            if(i!==terms-1)
-                questionString += generateRandomSign();
-        }
-        return questionString + " =";
-    }// end createQuestion
+//var answer = 0;
+function createQuestion(){
+    var questionString = "";
+    var terms = 2;
+    var numbers = new Array();
+    for(var i=0; i<terms; i++) {
+        numbers[i] = generateRandomNatrualNumber();
+        questionString += numbers[i];
+        if(i==0)
+           battle.setAnswer(numbers[i]);
+        else {
+            if(questionString.charAt(questionString.length-numbers[i].length-2)==='+')
+                battle.setAnswer(parseInt(battle.answer)+parseInt(numbers[i]));
+            if(questionString.charAt(questionString.length-numbers[i].length-2)==='-')
+                battle.setAnswer(parseInt(battle.answer)-parseInt(numbers[i]));
+            if(questionString.charAt(questionString.length-numbers[i].length-2)==='*')
+                battle.setAnswer(parseInt(battle.answer)*parseInt(numbers[i]));
+            if(questionString.charAt(questionString.length-numbers[i].length-2)==='/')// should round?
+                battle.setAnswer(parseInt(battle.answer)/parseInt(numbers[i]));
+        }// end if-else
+        if(i!==terms-1)
+            questionString += generateRandomSign();
+    }
+    return questionString + " =";
+}// end createQuestion
 
 
 
