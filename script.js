c92b00992ce274f84556281bccc901cac8a4dbcc
abcjjj <johnaotao@hotmail.com>
2013-11-05 23:18:31 -0500

    A major update; most codes updated to Jquery; the basic game is now functional.

--- script.js
+++ script.js
@@ -4,153 +4,84 @@
  * @since: Wednesday, October 30, 2013
  */
 
+
 /**
  * the Menu Screen
  * all elements are visible initially
  */
-function Menu() {
-    this.startButton = document.getElementById("start");
-    this.quitButton= document.getElementById("quit");
-    this.title= document.getElementById("title");
-    
-    function quitGame() {
-    // needs a BBUI function
-    }// end quitGame
-    
-    function startGame() {
-        document.getElementbyId("menu").style.visibility = "hidden";
-        document.getElementbyId("transition").style.visibility = "visible";
-        var character = new Character();
-    }// end startGame
-    
-}// end Constructor
-
-//Menu
-//initializes menu
-var menu = new Menu();
-
+function quitGame() {
+	// needs a BBUI function
+}// end quitGame
 // action events
-menu.startButton.onclick = menu.startGame();
-menu.quitButton.onclick = menu.quitGame();
-
+$(function(){
+	$("#start").click(function() {
+		$(".menu").css("visibility", "hidden");
+		$(".transitionSkip").css("visibility", "visible");
+	});
+});
+$(function(){
+	$("#quit").click(function() {
+		//needs BBUI function
+	});
+});
 
 /**
  * the character that the user controls
  */
 function Character() {
     this.money = 0;
-    
     function setMoney(money) {
         this.money = money;
     }// end mutatorbe
-    
 }// end Constructor
-
-//initializes character 
-    //(should go to another place?)
-
-// action events
-    // none (so far)
-
+var character = new Character();
 
 /**
  * the Transition Screen
  * elements become visible accordingly
  */
-function Transition() {
-    this.instructions = document.getElementById("instructions");
-    this.chooseToSkip = document.getElementById("chooseToSkip");
-    this.win = document.getElementById("win");
-    this.toNextSreen = this.toNextScreen = document.getElementById("toNextSccreen");
-    
-    function typeDisplayed(type) {
-        if(type==="instructions")
-            this.getanswerInstructions().style.visibility = "visible";
-        if(type==="before instructions")
-            this.getChooseToSkip().style.visibility = "visible";
-        if(type==="win")
-            this.getWin().style.visibility = "visible";
-        this.getToNextScreen().style.visibility = "visible";
-    }// end typeDisplayed
-    
-    function proceed(type) {
-        if(type==="instructions")
-            document.getElementById("battle").style.visibility = "visible";
-        if(type==="win")
-            document.getElementById("menu").style.visibility = "visible";
-        document.getElementById("transitionScreens").style.visibility = "hidden";
-    }// end proceed
-    
-}// end Constructor
-
-//Transition
-//initializes transition
-var transition = new Transition();
-
-// action events
-transition.chooseToSkip.onclick = transition.proceed("instructions");
-
-//proceed to different screens accordingly
-if($("#instructions").css("visibility")==="visible")
-    transition.getToNextScreen.onclick = transition.proceed("instructions");
-
-if($("#win").css("visibility")==="visible")
-    transition.getToNextScreen.onclick = transition.proceed("win");
-
+$(function(){
+	$("#yesView").click(function() {
+		$(".transitionSkip").css("visibility", "hidden");
+		$(".instruction").css("visibility", "visible");
+		$("#question").text(createQuestion(2, "noPower"));			
+	});
+	$("#noView").click(function() {
+		$(".transitionSkip").css("visibility", "hidden");
+		$(".battle").css("visibility", "visible");
+		$("#question").text(createQuestion(2, "noPower"));		
+	});
+});
+$(function(){
+	$("#ok").click(function() {
+		$(".instruction").css("visibility", "hidden");
+		$(".battle").css("visibility", "visible");
+	});
+});
+$(function(){
+	$("#backToMenu").click(function() {
+		$(".won").css("visibility", "hidden");
+		$(".menu").css("visibility", "visible");
+	});
+});
 
 /**
  * the battle screen
  */
-function Battle() {
-    this.answer = -1;
-    this.isCharacterDead = false;
-    this.background = document.getElementById("backgroundCave");
-    this.question = document.getElementById("question");
-    this.inputAnswer = document.getElementById("inputAnswer");
-    this.monster = document.getElementById("monster");
-    this.toShopButton = document.getElementById("toShop");
-    this.moneyValue = document.getElementById("showMoney");
-    
-    function setAnswer(answer) {
-        this.answer = answer;
-    }// end mutator
-    
-    function setIsCharacterKilled(dead) {
-        this.isCharacterDead = dead;
-    }// end mutator
+var answer = -1;
+var isCharacterDead = false;
+function setIsCharacterKilled(dead) {
+    this.isCharacterDead = dead;
+}// end mutator
+function createMonster(type) {
+    // some code to change the monster...
+}// end createMonster
+function death() {
+    // when something (?) is dead
+    document.write("Game Over");
+}// end death
 
-    function createMonster(type) {
-        // some code to change the monster...
-    }// end createMonster
-    
-    function death() {
-        // when something (?) is dead
-    }// end death
-    
-    function toShop() {
-        document.getElementById("battleScreen").style.visibility = "hidden";
-        document.getElementById("shop").style.visibility = "visible";
-    }// end toShop
-    
-    function answerSubmitted(isCorrect) {
-        if(isCorrect) {
-            character.money += 500;
-            
-        }
-        else {
-            this.death();
-        }// end if-else
-    }
-    
-    // make question
-    
-}// end Constructor
-
-//initializes battle screen 
-var battleScreen = new Battle();
-
-// @TO DO: add the followings to the above:
-
+//@TO DO: fix problems with the power terms
 /**
  * generates a random opeeration sign in an expression that will be generated
  * @ return a random opertation sign that might be +, -, *, or /
@@ -167,7 +98,6 @@ function generateRandomSign() {
     else //if(randomSign == 3)
         return " / ";  
 }// end generateRandomSign
-
 /**
  * generates a random 1-digit or 2-digit natrual number
  * @ return the number generated
@@ -178,16 +108,14 @@ function generateRandomNatrualNumber() {
     else // 1 digit
         return "" + Math.round(Math.random()*10);
 }// end generateRandomSign
-
 /**
  * a function that creates a power (i.e. 2^3)
  * base and exponent should be 1-digit; base could be 10
  * @ return a string in the form of base^exponent
  */
 function generateRandomPower() {
-    return Math.round(Math.random()*10) + "^" + Math.floor(Math.random()*10);
+    return Math.floor(Math.random()*10) + "^" + Math.floor(Math.random()*10);
 }// end generateRandomPower
-
 /**
  * turns a power into an integer
  * @ param power is the power that is to be transformed
@@ -196,7 +124,6 @@ function generateRandomPower() {
 function turnPowerIntoNumber(power) {
     return Math.pow(parseInt(power.charAt(0)), power.charAt(2)); //at index 0 is the base, at index 1 is '^', and at index 2 is the exponent
 }// end turnPowerIntoNumber
-    
 //var answer = 0;
 /**
  * creates the an expression that the user will evaluate
@@ -204,6 +131,7 @@ function turnPowerIntoNumber(power) {
  * @ param type what type of terms will exsist within the expression
  * @ return the question randomly generated according to the parameters
  */
+//@TO DO prevent 0 from becoming the denomenator
 function createQuestion(term, type){
     var questionString = "";// would mainly consists of the expression
     var terms = term; // should be set to 2 for now
@@ -231,16 +159,16 @@ function createQuestion(term, type){
         }// end if-else
         
         if(i==0)
-              battle.setAnswer(numbers[i]);
+              answer=(numbers[i]);
         else {
             if(questionString.charAt(questionString.length-numbers[i].length-2)==='+')
-                battle.setAnswer(parseInt(battle.answer)+parseInt(numbers[i]));
+                answer=(parseInt(answer)+parseInt(numbers[i]));
             if(questionString.charAt(questionString.length-numbers[i].length-2)==='-')
-                battle.setAnswer(parseInt(battle.answer)-parseInt(numbers[i]));
+                answer=(parseInt(answer)-parseInt(numbers[i]));
             if(questionString.charAt(questionString.length-numbers[i].length-2)==='*')
-                battle.setAnswer(parseInt(battle.answer)*parseInt(numbers[i]));
+                answer=(parseInt(answer)*parseInt(numbers[i]));
             if(questionString.charAt(questionString.length-numbers[i].length-2)==='/')// should round to 1-decimal place
-                battle.setAnswer(parseInt(battle.answer)/parseInt(numbers[i]));
+                answer=(parseInt(answer)/parseInt(numbers[i]));
         }// end if-else
         if(i!==terms-1)
             questionString += generateRandomSign();
@@ -248,69 +176,44 @@ function createQuestion(term, type){
     
     return questionString + " = ?";
 }// end createQuestion
-
 /**
  * checks whether if the answer from the user is right
  * @ decimal how many decimal places the generated answer is rounded to
  * @ return whether if the answer is correct
  */
 function checkAnswer(decimal) {
-    return $("#inputAnswer").val() === Math.round(battle.answer*(Math.pow(10, decimal))/(Math.pow(10, decimal));
+    return parseFloat($("#userAnswer").val()) === Math.round(answer*(Math.pow(10, decimal))/(Math.pow(10, decimal)));
 }// end checkAnswer
-
-
-// action events
-battle.toShopButton.onclick = battle.toShop();
-
-//need a submit in a form
-$("#submit").click(checkAnswer());
-
-
+$(function(){
+	$("#submitAnswer").click(function() {
+		alert(checkAnswer(1));
+		if(checkAnswer(1)) {
+			character.money += 500;
+			$("#showMoney").text("Money: $" + character.money);
+			$("#question").text(createQuestion(2, "noPower"));	
+		}
+		else
+			death();
+	});
+});
 
 /**
  * the shop
  */
-function Shop() {
-    var itemsDisplayed = document.getElementById("itemsDisplayed");// for now,this only includes the key
-    var returnToBattle = document.getElementById("returnToBattle");// Yet to be included
-    var isKeyBought = false;
-    var isGameWon = false;
-    
-    function setIsKeyBought(bought) {
-        isKeyBought = is;
-    }// end mutator
-    
-    function setIsGameWon(won) {
-        isGameWon = won;
-    }// end mutator
-    
-    function gameWon() {
-        $("#shop").css("visibility", "hidden");
-        transition.proceed("win");
-    }// end gameWon
-    
-    function returnToBattleScreen() {
-        $("#shop").css("visibility", "hidden");
-        $("#battle".css("visibility", "visible");)
-    }// end returnToBattle
-    
-}// end constructor
-
-// initialize Shop
-var shop = new Shop();
-
-// actions events
-shop.returnToBattle.onclick = shop.returnToBattleScreen();
-
-if(character.money>==1000)//temp.
-    shop.itemDisplayed.onclick = shop.setIsKeyBought(true);
-// there could be intermediate steps
-if(shop.isKeyBought===true)
-    shop.setIsGameWon(true);
-if(shop.isGameWon===true)
-    shop.gameWon();
-
-
-
-
+$(function(){
+	$("#toShop").click(function() {
+		$(".battle").css("visibility", "hidden");
+		$(".shop").css("visibility", "visible");
+	});
+	$("#toBattle").click(function() {
+		$(".shop").css("visibility", "hidden");
+		$(".battle").css("visibility", "visible");
+	});
+	$("#key").click(function() {
+		if(character.money>=500) {
+			$(".shop").css("visibility", "hidden");
+			$(".won").css("visibility", "visible");
+		}
+	});
+});
 
